b56711242f794492534eb14717fc8506a8c27a41
Parthipan Sekar <parthipan.sekar@xe.com>
2021-04-20 10:20:24 -0400

    index is setup up

---
+++ pages/room/[id].js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from "react";
+
+function RoomPage(props) {
+  return (
+    <>
+      <Head>
+        <title>ROOM PAGE</title>
+      </Head>
+      <p>ROOM</p>
+    </>
+  );
+}
+
+export default RoomPage;
