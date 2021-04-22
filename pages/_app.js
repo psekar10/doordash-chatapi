df5628c74e03486183333ca0abf53c3656913ec7
Parthipan Sekar <parthipan.sekar@xe.com>
2021-04-21 23:16:42 -0400

    update the app js

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,8 @@
-import Head from "next/head"
 import React from "react";
-import { Global, css } from '@emotion/react'
 
 function MyApp({ Component, pageProps }) {
   return (
 	<>
-		<Global
-			styles={css`
-				html,
-				body {
-					padding: 0;
-					margin: 0;
-					font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-						Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-						sans-serif;
-				}
-
-				* {
-					box-sizing: border-box;
-				}
-			`}
-		/>
 		<Component {...pageProps} />
 	</>
 	)
